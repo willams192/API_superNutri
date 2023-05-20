19490140bfb2b68c9fcf3b4358aa3fed4cbfecdc
willams192 <wiilhams82@gmail.com>
2023-05-20 17:28:21 -0300

    Adicionando o imports, validações e mesagens de erro para tratar os campos dos reponsáveis

--- Routes/reponsavel.route.js
+++ Routes/reponsavel.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const responsavel = require('../Models/Responsavel');
 const app = express();
 const responsavelRoutes = express.Router();
-
+const { validarNome, validarSenha, validarEmail } = require('../Validation/validationsResponsavel');
 
 
 let Responsavel = require('../Models/Responsavel')
@@ -10,14 +10,22 @@ let Responsavel = require('../Models/Responsavel')
 // adiciona o usuário
 responsavelRoutes.route('/add').post(async function (req, res) {
     let responsavel = new Responsavel(req.body);
-    console.log(responsavel)
-    responsavel.save(req.body)
-        .then(result => {
-            res.status(200).json({ 'status': 'sucess', 'msg': 'usuário cadastrado com sucesso' });
-        })
-        .catch(err => {
-            res.status(409).json({ 'status': 'failure', 'msg': 'usuário não cadastrado' + err.message })
-        });
+    try {
+        validarEmail(req.body.email);
+        validarNome(req.body.nome);
+        validarSenha(req.body.senha);
+
+
+        responsavel.save(req.body)
+            .then(result => {
+                res.status(200).json({ 'status': 'sucess', 'msg': 'usuário cadastrado com sucesso' });
+            })
+            .catch(err => {
+                res.status(409).json({ 'status': 'failure', 'msg': 'usuário não cadastrado' + err.message })
+            });
+    } catch (error) {
+        res.status(400).json({ 'status': 'failure', 'msg': 'Não foi possivel cadastrar esse usuário' });
+    };
 });
 
 
