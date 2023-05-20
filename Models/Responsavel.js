e1bc658ebafcb35bd0e207c2b62dbc3c4202abe6
willams192 <wiilhams82@gmail.com>
2023-05-20 17:24:05 -0300

    trocando o nome dos campos

--- Models/Responsavel.js
+++ Models/Responsavel.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let userResponsavel = new Schema({
-    Nome: {
+    nome: {
         type: String
     },
-    Senha: {
+    senha: {
         type: String
     },
     email: {
